782ce3a3474dd01a71c0a0216fee866ef4ff76f7
noribozo <krasznaynori@gmail.com>
2021-03-12 09:43:51 +0100

    Create a React app and publish it to GitHub pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from "react"
+import './index.css';
 
 function App() {
+  const startingTime = 15
+
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(startingTime)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
+  const textBoxRef = useRef(null)
+
+  function handleChange (event) {
+    const {value} = event.target
+    setText(value)
+  }
+   function calculateWordCount(text){
+     const wordsArr = text.trim().split(" ")
+     return wordsArr.filter(word => word !== "").length
+   }
+
+   function startGame() {
+     setIsTimeRunning(true)
+     setTimeRemaining(startingTime)
+     setText("")
+     setWordCount(0)
+     textBoxRef.current.disabled = false
+     textBoxRef.current.focus()
+   }
+
+   function endGame() {
+     setIsTimeRunning(false)
+     setWordCount(calculateWordCount(text))
+   }
+
+   useEffect(() => {
+      if(isTimeRunning && timeRemaining > 0) {
+        setTimeout(() => {
+          setTimeRemaining(time => time -1)
+        }, 1000)
+      } else if(timeRemaining === 0) {
+        endGame()
+      }
+   }, [timeRemaining, isTimeRunning])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>How fast can you type?</h1>
+      <textarea 
+          ref={textBoxRef}
+          onChange={handleChange}
+          value={text}
+          disabled={!isTimeRunning}
+          />
+      <h1>Time remaining: {timeRemaining}</h1>
+      <button
+          onClick={startGame}
+          disabled={isTimeRunning}>Start</button>
+      <h1>Word count: {wordCount}</h1>
+     
     </div>
   );
 }
